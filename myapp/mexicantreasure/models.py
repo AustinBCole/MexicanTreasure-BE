491b9d90b843c297ef20b2334ef12b984d09834d
AustinBCole <austin.cole.chileno@gmail.com>
2019-09-24 14:07:11 -0600

    Initialize Django project, begin with models.

--- myapp/mexicantreasure/models.py
+++ myapp/mexicantreasure/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Player(models.Model) {
+    user_uuid = models.IntegerField()
+    username = models.CharField(max_length=None)
+    password = models.CharField(max_length=None)
+    has_active_session = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{username} {user_uuid}" 
+}
+
+class Session(models.Model) {
+    session_uuid = models.IntegerField()
+    session_status = models.CharField(max_length=None)
+    players = models.ManyToManyField(Player)
+
+}
