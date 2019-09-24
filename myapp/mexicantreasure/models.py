4fcaada714df0c542d83ffa06741bf5d359632c4
AustinBCole <austin.cole.chileno@gmail.com>
2019-09-24 14:46:02 -0600

    Player object ready to roll.

--- myapp/mexicantreasure/models.py
+++ myapp/mexicantreasure/models.py
@@ -2,19 +2,18 @@ from django.db import models
 
 # Create your models here.
 
-class Player(models.Model) {
+class Player(models.Model):
     user_uuid = models.IntegerField()
-    username = models.CharField(max_length=None)
-    password = models.CharField(max_length=None)
+    username = models.CharField(max_length=10000)
+    password = models.CharField(max_length=10000)
     has_active_session = models.BooleanField(default=False)
+    friends = models.ForeignKey("self", on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{username} {user_uuid}" 
-}
+        return f"{self.user_uuid} {self.username}" 
 
-class Session(models.Model) {
+
+class Session(models.Model): 
     session_uuid = models.IntegerField()
-    session_status = models.CharField(max_length=None)
+    session_status = models.CharField(max_length=10000)
     players = models.ManyToManyField(Player)
-
-}
